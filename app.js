04c7195a18b7f7ab561a9c61f7ca033293d3c630
Shaikh-Danish <shaikhsaalim327@gmail.com>
2026-06-16 22:05:52 +0530

    feat: initialize node express application with docker and github actions deployment workflow

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+console.log('Starting server...');
+
+app.get('/', (req, res) => {
+  res.send('Hello from server!');
+});
+
+app.listen(3000, () => {
+  console.log('Server running');
+});
